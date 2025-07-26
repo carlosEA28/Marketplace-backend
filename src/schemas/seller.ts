d091add4f94e4e2064b395f560524b5cd93ed066
carlosEA28 <carloseaduarte28@gmail.com>
2025-07-26 17:07:40 -0300

    feat: add PostgresCreateSellerRepository

---
+++ src/schemas/seller.ts
@@ -0,0 +1,40 @@
+import { z } from "zod";
+import parsePhoneNumberFromString from "libphonenumber-js";
+
+export const CreateSellerProps = z.object({
+  full_name: z.string().min(1, { message: "O nome completo é obrigatório" }),
+  phone: z.string().superRefine((br, ctx) => {
+    const phone = parsePhoneNumberFromString(br, {
+      defaultCountry: "BR",
+      extract: false,
+    });
+
+    if (!phone || !phone.isValid()) {
+      ctx.addIssue({
+        code: "custom",
+        message: "Número de telefone inválido",
+      });
+    }
+  }),
+  email: z
+    .email({ message: "Email inválido" })
+    .trim()
+    .min(1, { message: "O email é obrigatório" }),
+  password: z
+    .string()
+    .trim()
+    .min(6, { message: "A senha tem que conter no mínimo 6 caractéres" }),
+  cpf: z
+    .string({
+      message: "CPF/CNPJ é obrigatório.",
+    })
+    .refine((doc) => {
+      const replacedDoc = doc.replace(/\D/g, "");
+      return replacedDoc.length >= 11;
+    }, "CPF deve conter no mínimo 11 caracteres.")
+    .refine((doc) => {
+      const replacedDoc = doc.replace(/\D/g, "");
+      return !!Number(replacedDoc);
+    }, "CPF deve conter apenas números."),
+  avatarImg: z.string().optional(),
+});
