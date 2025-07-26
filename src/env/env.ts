69b0fcfc9da8559bed68560a634e522f0608aed8
carlosEA28 <carloseaduarte28@gmail.com>
2025-07-26 19:01:42 -0300

    feat: add envSchema parsing with zod

---
+++ src/env/env.ts
@@ -0,0 +1,12 @@
+import z from "zod";
+
+const envSchema = z.object({
+  DATABASE_URL: z.url(),
+  POSTGRES_USER: z.string(),
+  POSTGRES_PASSWORD: z.string(),
+  POSTGRES_DB: z.string(),
+  JWT_ACCESS_TOKEN_SECRET: z.string(),
+  JWT_REFRESH_TOKEN_SECRET: z.string(),
+});
+
+export const env = envSchema.parse(process.env);
