5dc239f0fd345718ab5680e0d92df752626a71a6
carlosEA28 <carloseaduarte28@gmail.com>
2025-07-27 10:15:12 -0300

    feat: add CreateSellerService

---
+++ src/service/seller/create_seller.ts
@@ -0,0 +1,53 @@
+import z from "zod";
+import { CreateSellerProps } from "../../schemas/seller";
+import { PostgresGetSellerByEmailRepository } from "../../repositories/postgres/seller/getSellerByEmail";
+import { PostgresCreateSellerRepository } from "../../repositories/postgres/seller/create_seller";
+import { PasswordEncoderAdapter } from "../../adapters/passwordEncoder";
+import { TokenGeneratorAdapter } from "../../adapters/tokenGenerator";
+import { v4 as uuidV4 } from "uuid";
+
+type CreateSellerData = z.infer<typeof CreateSellerProps>;
+
+export class CreateSellerService {
+  constructor(
+    private getSellerByEmailRepository: PostgresGetSellerByEmailRepository,
+    private createSellerRepository: PostgresCreateSellerRepository,
+    private passwordEncoderAdapter: PasswordEncoderAdapter,
+    private tokensGeneratorAdapter: TokenGeneratorAdapter
+  ) {
+    this.getSellerByEmailRepository = getSellerByEmailRepository;
+    this.createSellerRepository = createSellerRepository;
+    this.passwordEncoderAdapter = passwordEncoderAdapter;
+    this.tokensGeneratorAdapter = tokensGeneratorAdapter;
+  }
+
+  async execute(createSellerParams: CreateSellerData) {
+    const sellerAlreadyExists = await this.getSellerByEmailRepository.execute(
+      createSellerParams.email
+    );
+
+    if (sellerAlreadyExists) {
+      //criar erro customizado
+      throw new Error("Vendedor ja cadastrado");
+    }
+
+    const sellerId = uuidV4();
+
+    const hashedPassoword = await this.passwordEncoderAdapter.execute(
+      createSellerParams.password
+    );
+
+    const newSeller = {
+      ...createSellerParams,
+      id: sellerId,
+      password: hashedPassoword,
+    };
+
+    const createdSeller = await this.createSellerRepository.execute(newSeller);
+
+    return {
+      ...createdSeller,
+      tokens: await this.tokensGeneratorAdapter.execute(sellerId),
+    };
+  }
+}
