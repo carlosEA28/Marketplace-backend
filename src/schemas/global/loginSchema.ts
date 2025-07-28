25c7934670f5f7269d9a3a57a5dd4aa604d9465a
carlosEA28 <carloseaduarte28@gmail.com>
2025-07-28 11:51:08 -0300

    feat:add loginSchema

---
+++ src/schemas/global/loginSchema.ts
@@ -0,0 +1,11 @@
+import z from "zod";
+
+export const loginSchema = z.object({
+  email: z.email({
+    error: "Email is required",
+  }),
+
+  password: z.string({
+    error: "Password is required",
+  }),
+});
