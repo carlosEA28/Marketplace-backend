ea1ca3780f3202192cb1cc0cc47e9c1c077ab21e
carlosEA28 <carloseaduarte28@gmail.com>
2025-07-28 11:05:32 -0300

    feat: add UserNotFoundError and InvalidPasswordError

--- src/errors/seller.ts
+++ src/errors/seller.ts
@@ -4,3 +4,16 @@ export class EmailAlreadyInUseError extends Error {
     this.name = "EmailAlreadyInUse";
   }
 }
+export class UserNotFoundError extends Error {
+  constructor() {
+    super(`O usuário  não foi encontrado`);
+    this.name = "UserNotFoundError";
+  }
+}
+
+export class InvalidPasswordError extends Error {
+  constructor() {
+    super("A senha fornecida é inválida");
+    this.name = "InvalidPasswordError";
+  }
+}
