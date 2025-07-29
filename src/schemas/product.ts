94e336e85ce9894394baa703911acb83765cf97c
carlosEA28 <carloseaduarte28@gmail.com>
2025-07-29 10:27:17 -0300

    feat: ensure  sellerId, categoryId are strings

--- src/schemas/product.ts
+++ src/schemas/product.ts
@@ -2,15 +2,15 @@ import z from "zod";
 
 export const CreateProductSchema = z.object({
   title: z.string().min(1, { message: "O título é obrigatório" }),
-  price: z
-    .number({ error: "O preço deve ser um número" })
-    .positive({ message: "O preço deve ser um número positivo" }),
-  sellerId: z.uuid({ message: "ID do vendedor inválido" }),
-  categoryId: z.uuid({ message: "ID da categoria inválido" }),
-  type: z.enum(["ANUNCIADO", "VENDIDO", "DESATIVADO"], {
-    message: "Tipo de produto inválido",
-  }),
-  productImage: z.url({ message: "URL da imagem inválida" }),
+  price: z.string({ error: "O preço é obrigatório" }),
+  sellerId: z.string({ message: "ID do vendedor inválido" }),
+  categoryId: z.string({ message: "ID da categoria inválido" }),
+  type: z
+    .enum(["ANUNCIADO", "VENDIDO", "DESATIVADO"], {
+      message: "Tipo de produto inválido",
+    })
+    .default("ANUNCIADO"),
+  productImage: z.url({ message: "URL da imagem inválida" }).optional(),
 });
 
 export type CreateProductData = z.infer<typeof CreateProductSchema>;
