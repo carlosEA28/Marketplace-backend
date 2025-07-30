5d4690c146dbf2fa4114f699fd528014cd27bfa1
carlosEA28 <carloseaduarte28@gmail.com>
2025-07-30 10:05:50 -0300

    feat: add getAllAnouncedProductsQuantityFactory

--- src/factories/controllers/product.ts
+++ src/factories/controllers/product.ts
@@ -4,6 +4,10 @@ import { PostgresGetSellerById } from "../../repositories/postgres/seller/getSel
 import { S3Service } from "../../service/s3";
 import { CreateProductService } from "../../service/product/create_product";
 import { CreateProductController } from "../../controllers/product/create_product";
+import { PostgreGetAllAnoucedProductsQuantityRepository } from "../../repositories/postgres/product/getAllAnoucedProducts";
+import { GetAllAnoucedProductsQuantityController } from "../../controllers/product/getAllAnouncedProducts";
+import { GetAllAnoucedProductsQuantityService } from "../../service/product/getAllAnoucedProducts";
+// import PostgreGetAllAnoucedProductsRepository
 
 export const makeCreateProductController = () => {
   const getSellerByIdRepository = new PostgresGetSellerById();
@@ -21,4 +25,21 @@ export const makeCreateProductController = () => {
   );
 
   return createProductController;
+};
+
+export const makeGetAllAnoucedProductsQuantityController = () => {
+  const getSellerByIdRepository = new PostgresGetSellerById();
+  const getAllAnouncedProductsRepository =
+    new PostgreGetAllAnoucedProductsQuantityRepository();
+
+  const getAllAnouncedProductsService =
+    new GetAllAnoucedProductsQuantityService(
+      getSellerByIdRepository,
+      getAllAnouncedProductsRepository
+    );
+
+  const getAllAnouncedProductsController =
+    new GetAllAnoucedProductsQuantityController(getAllAnouncedProductsService);
+
+  return getAllAnouncedProductsController;
 };
