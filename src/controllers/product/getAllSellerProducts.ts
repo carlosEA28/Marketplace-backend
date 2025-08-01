44e558d7a7c79233df15c6a9c6893cfe83cad000
carlosEA28 <carloseaduarte28@gmail.com>
2025-08-01 08:07:14 -0300

    feat: add getAllSellerProductsController

---
+++ src/controllers/product/getAllSellerProducts.ts
@@ -0,0 +1,37 @@
+import { Request } from "express";
+import { GetAllSellerProductsService } from "../../service/product/getAllProducts";
+import { ZodError } from "zod";
+import { badRequest, notFound, ok, serverError } from "../helpers/httpHelper";
+import { UserNotFoundError } from "../../errors/seller";
+
+export class GetAllSellerProductsController {
+  constructor(private getAllSellerProducts: GetAllSellerProductsService) {
+    this.getAllSellerProducts = getAllSellerProducts;
+  }
+
+  async execute(httpRequest: Request) {
+    try {
+      const { sellerId } = httpRequest.params;
+
+      const products = await this.getAllSellerProducts.execute(sellerId);
+
+      return ok(products);
+    } catch (error) {
+      if (error instanceof ZodError) {
+        return badRequest({
+          message: error.message,
+        });
+      }
+
+      if (error instanceof UserNotFoundError) {
+        return notFound({
+          message: error.message,
+        });
+      }
+
+      console.log(error);
+
+      return serverError("Erro do servidor");
+    }
+  }
+}
