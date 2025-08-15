3ab7c5ee71943dd96456254f32e949d103d763cc
carlosEA28 <carloseaduarte28@gmail.com>
2025-08-15 10:21:21 -0300

    test: ensure prisma return null if the email does not exist

---
+++ src/repositories/postgres/seller/getSellerByEmail.test.ts
@@ -0,0 +1,34 @@
+import { seller } from "../../../tests/fixtures/seller";
+import { beforeEach, describe, expect, it } from "@jest/globals";
+import { PostgresCreateSellerRepository } from "./create_seller";
+import { PostgresGetSellerByEmailRepository } from "./getSellerByEmail";
+import { prisma } from "../../../../prisma/prisma";
+
+const createSeller = async () => {
+  const sellerRepository = new PostgresCreateSellerRepository();
+  const newSeller = await sellerRepository.execute(seller);
+
+  return newSeller;
+};
+
+describe("Get seller by Email repository", () => {
+  beforeEach(async () => {
+    await prisma.seller.deleteMany({});
+  });
+
+  it("should get seller by email from db", async () => {
+    const newSeller = createSeller();
+    const sut = new PostgresGetSellerByEmailRepository();
+
+    const res = await sut.execute((await newSeller).email);
+
+    expect(res?.email).toBe((await newSeller).email);
+  });
+
+  it("should return null if the email does not exist", async () => {
+    const sut = new PostgresGetSellerByEmailRepository();
+    const res = await sut.execute("emailnaoexistente@example.com");
+
+    expect(res).toBeNull();
+  });
+});
