2f8e64ef32b8f8f2b5f5ca74b9f180ae28d62b03
carlosEA28 <carloseaduarte28@gmail.com>
2025-08-15 09:31:44 -0300

    test: ensure prisma creates a seller on db and prisma is called with the correct params

--- src/repositories/postgres/seller/createSeller.test.ts
+++ src/repositories/postgres/seller/createSeller.test.ts
@@ -1,19 +1,46 @@
 import { PostgresCreateSellerRepository } from "./create_seller";
 import { faker } from "@faker-js/faker";
 import { prisma } from "../../../../prisma/prisma";
-import { describe, expect, it, beforeEach } from "@jest/globals";
+import { describe, expect, it, beforeEach, jest } from "@jest/globals";
 import { seller } from "../../../tests/fixtures/seller";
+import { cpf } from "cpf-cnpj-validator";
 
 describe("Create user repository", () => {
   beforeEach(async () => {
     await prisma.seller.deleteMany({});
   });
 
-  it("should create a user on db", async () => {
+  it("should create a seller on db", async () => {
     const sut = new PostgresCreateSellerRepository();
 
     const res = await sut.execute(seller);
 
-    expect(res).toBeTruthy();
+    expect(res.id).toBeDefined();
+    expect(res.id).toHaveLength(36);
+    expect(res.full_name).toBe(seller.full_name);
+    expect(res.phone).toBe(seller.phone);
+    expect(res.email).toBe(seller.email);
+    expect(res.password).toBe(seller.password);
+    expect(res.avatarImage).toBe(seller.avatarImg);
+  });
+
+  it("should call Prisma with correct params", async () => {
+    const sut = new PostgresCreateSellerRepository();
+
+    const prismaSpy = jest.spyOn(prisma.seller, "create");
+
+    await sut.execute(seller);
+
+    expect(prismaSpy).toHaveBeenCalledWith({
+      data: expect.objectContaining({
+        id: expect.any(String),
+        full_name: seller.full_name,
+        phone: seller.phone,
+        email: seller.email,
+        password: seller.password,
+        cpf: seller.cpf,
+        avatarImage: seller.avatarImg,
+      }),
+    });
   });
 });
--- src/tests/fixtures/seller.ts
+++ src/tests/fixtures/seller.ts
@@ -1,10 +1,11 @@
 import { faker } from "@faker-js/faker";
 
 export const seller = {
+  id: faker.string.uuid(),
   full_name: faker.person.firstName(),
   phone: "5199989937",
   email: faker.internet.email(),
   password: faker.internet.password(),
   cpf: "60106842005",
-  avatarImg: faker.string.uuid(),
+  avatarImg: faker.string.alphanumeric(),
 };
